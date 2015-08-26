fdf6aa7c7cc7e6e3b7101f0d39a8fba37eaca7b1
Shlok Amin <shlok.amin1994@gmail.com>
2015-08-26 10:22:19 -0500

    added microblogs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def hello
-    render text: "hello, world!"
-
-
-    Gchart.bar( :data => [[1,2,4,67,100,41,234],[45,23,67,12,67,300, 250]], 
-            :title => 'SD Ruby Fu level', 
-            :legend => ['matt','patrick'], 
-            :bg => {:color => '76A4FB', :type => 'gradient'}, 
-            :bar_colors => 'ff0000,00ff00')
-
-  end
-
 end
